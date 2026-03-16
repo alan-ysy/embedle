7c3dd8cff1017cd4e77b40cd14e97bb65b53da12
Alan <184780131+alan-ysy@users.noreply.github.com>
2026-03-16 15:23:57 -0400

    Create main() function

--- word_processor.py
+++ word_processor.py
@@ -1,10 +1,13 @@
 import json
 
-with open('word_lists/main_list.txt', 'r') as word_list_file:
-    word_list = word_list_file.readlines()
+def main():
+    cluster_dict = {}
 
-    for word in word_list:
-        pass
+    with open('word_lists/main_list.txt', 'r') as word_list_file:
+        word_list = word_list_file.readlines()
+
+        for word in word_list:
+            word_clusters = find_clusters(word)
 
 # Takes in a string and a cluster length, returns a list of all clusters that word contains
 def find_clusters(word, cluster_length=3):
@@ -16,3 +19,6 @@ def find_clusters(word, cluster_length=3):
         cluster_list.append(word[i:i+cluster_length])
     
     return cluster_list
+
+if __name__ == '__main__':
+    main()
