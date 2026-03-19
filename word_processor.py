9904f58ca348b1eb449eced2ff26b7181c3c4c18
Alan <184780131+alan-ysy@users.noreply.github.com>
2026-03-19 13:24:10 -0400

    Clean up formatting

--- word_processor.py
+++ word_processor.py
@@ -1,15 +1,16 @@
 import json
 
+
 def main():
     # Store as cluster : [word list]
     cluster_dict = {}
 
     # Read word list and parse clusters
-    with open('word_lists/main_list.txt', 'r') as word_list_file:
+    with open("word_lists/main_list.txt", "r") as word_list_file:
         for word in word_list_file:
-            word = word.strip()                         # Remove trailing '\n'
+            word = word.strip()  # Remove trailing '\n'
 
-            word_clusters = set(find_clusters(word))    # Use set to avoid duplication
+            word_clusters = set(find_clusters(word))  # Use set to avoid duplication
             for cluster in word_clusters:
                 if cluster in cluster_dict:
                     cluster_dict[cluster].append(word)
@@ -19,22 +20,24 @@ def main():
                     # print(f'CREATE: {cluster} : {word}')
 
     # Save to JSON file
-    with open('word_lists/clusters.json', 'w') as cluster_file:
+    with open("word_lists/clusters.json", "w") as cluster_file:
         json.dump(cluster_dict, cluster_file, indent=2)
+
 
 # Takes in a string and a cluster length, returns a list of all clusters that word contains
 def find_clusters(word, cluster_length=3):
     if len(word) < cluster_length:
         return []
-    
+
     num_clusters = len(word) - (cluster_length - 1)
 
     cluster_list = []
 
     for i in range(num_clusters):
-        cluster_list.append(word[i:i+cluster_length])
-    
+        cluster_list.append(word[i : i + cluster_length])
+
     return cluster_list
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
